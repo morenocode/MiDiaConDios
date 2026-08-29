5fe1476eca2cb882541a9e182f584e1456960fb0
Bryan R Moreno Duran <63120377+morenocode@users.noreply.github.com>
2026-08-28 20:39:05 -0500

    Bump app to v0.9.1 reset build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.modu.midiacondios"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.9.0"
+        versionCode = 10
+        versionName = "0.9.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
