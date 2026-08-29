749d90b25c2356d3ad062dd96b8fff7f2ef26d55
Bryan R Moreno Duran <63120377+morenocode@users.noreply.github.com>
2026-08-29 08:55:22 -0500

    Bump app to v0.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.modu.midiacondios"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.12.0"
+        versionCode = 14
+        versionName = "0.13.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
