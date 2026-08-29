f86e609972cc847d50e44b44168bfe2f25aa355c
Bryan R Moreno Duran <63120377+morenocode@users.noreply.github.com>
2026-08-28 20:38:50 -0500

    Use resettable v0.9 home screen

--- app/src/main/java/com/modu/midiacondios/V07Activity.kt
+++ app/src/main/java/com/modu/midiacondios/V07Activity.kt
@@ -141,7 +141,7 @@ fun MiDiaConDiosV07App() {
                 modifier = Modifier.then(Modifier)
             ) {
                 composable("inicio") {
-                    V09HomeScreen(
+                    V09ResettableHomeScreen(
                         contentPadding = padding,
                         onPrayers = { navigateTopLevel("oraciones") },
                         onGratitude = { nav.navigate("gratitud") { launchSingleTop = true } },
